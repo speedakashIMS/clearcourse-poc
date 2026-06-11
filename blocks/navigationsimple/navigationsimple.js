3c1734eb0180f7e1b60d287510902201602ee3f7
Ali Kurtulush Admin <alik_admin@imsolutions.net>
2026-06-12 00:37:51 +0100

    updated header changes

--- blocks/navigationsimple/navigationsimple.js
+++ blocks/navigationsimple/navigationsimple.js
@@ -35,7 +35,7 @@ export default async function decorate(block) {
   let navItems = [];
   Array.from(block.children).forEach((row) => {
     const cells = Array.from(row.children);
-    if (cells[0]?.textContent.trim().toLowerCase() === 'submenuitems') {
+    if (cells[0]?.textContent.trim().toLowerCase() === 'navigationitems') {
       const ul = cells[1]?.querySelector('ul');
       if (ul) navItems = parseNavLinks(ul);
     }
